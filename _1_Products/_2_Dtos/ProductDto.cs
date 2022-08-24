0cf5ad9fd4b81f33dcc19de75c71122eebf31efe
ahmady016 <ahmady09@gmail.com>
2022-08-24 13:17:35 +0200

    Add missing Id prop in ProductDto

--- _1_Products/_2_Dtos/ProductDto.cs
+++ _1_Products/_2_Dtos/ProductDto.cs
@@ -3,6 +3,7 @@ using Entities;
 namespace Dtos;
 public class ProductDto
 {
+  public Guid Id { get; set; }
   public string Title { get; set; }
   public string Description { get; set; }
   public Category Category { get; set; }
