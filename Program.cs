8532b5d04eb92000b54f06f348d6163b3fd5ac08
ahmady016 <ahmady09@gmail.com>
2022-08-24 11:14:58 +0200

    init app state

---
+++ Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.Run();
