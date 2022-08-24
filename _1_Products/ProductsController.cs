035edcfc4690483abdca6236ead7d80166fcdbd5
ahmady016 <ahmady09@gmail.com>
2022-08-24 12:39:13 +0200

    Refactor to using AsParameters

--- _1_Products/ProductsController.cs
+++ _1_Products/ProductsController.cs
@@ -16,45 +16,43 @@ public class ProductsController : ControllerBase
     _mediator = mediator;
   }
 
-    /// <summary>
-    /// listType values (all/deleted/existed)
-    /// Products/List?type=existed
-    /// </summary>
-    /// <returns>List of ProductDto</returns>
-    [HttpGet]
-    public Task<IResult> List(string type, int? pageSize, int? pageNumber)
-    {
-        return _mediator.Send(new ListProductQuery() { ListType = type, PageSize = pageSize, PageNumber = pageNumber });
-    }
+  /// <summary>
+  /// listType values (all/deleted/existed)
+  /// Products/List?type=existed
+  /// </summary>
+  /// <returns>List of ProductDto</returns>
+  [HttpGet]
+  public Task<IResult> List([AsParameters] ListProductQuery query)
+  {
+      return _mediator.Send(query);
+  }
+  /// <summary>
+  /// Products/Find/[id]
+  /// </summary>
+  /// <returns>ProductDto</returns>
+  [HttpGet("{id}")]
+  public Task<IResult> Find([AsParameters] FindProductQuery query)
+  {
+      return _mediator.Send(query);
+  }
 
-    /// <summary>
-    /// Products/Find/[id]
-    /// </summary>
-    /// <returns>ProductDto</returns>
-    [HttpGet("{id}")]
-    public Task<IResult> Find(Guid id)
-    {
-        return _mediator.Send(new FindProductQuery() { Id = id });
-    }
-
-    /// <summary>
-    /// Products/Add
-    /// </summary>
-    /// <returns>ProductDto</returns>
-    [HttpPost]
-    public Task<IResult> Add(AddProductCommand input)
-    {
-        return _mediator.Send(input);
-    }
-
-    /// <summary>
-    /// Products/Update
-    /// </summary>
-    /// <returns>ProductDto</returns>
-    [HttpPost]
-    public Task<IResult> Update(UpdateProductCommand input)
-    {
-        return _mediator.Send(input);
-    }
+  /// <summary>
+  /// Products/Add
+  /// </summary>
+  /// <returns>ProductDto</returns>
+  [HttpPost]
+  public Task<IResult> Add(AddProductCommand input)
+  {
+      return _mediator.Send(input);
+  }
+  /// <summary>
+  /// Products/Update
+  /// </summary>
+  /// <returns>ProductDto</returns>
+  [HttpPost]
+  public Task<IResult> Update(UpdateProductCommand input)
+  {
+      return _mediator.Send(input);
+  }
 
 }
