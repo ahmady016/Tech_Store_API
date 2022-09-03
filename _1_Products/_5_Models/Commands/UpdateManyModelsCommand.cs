f7c083161c99214d3f8f16b0ea03929b8100b233
ahmady016 <ahmady09@gmail.com>
2022-09-03 22:11:44 +0200

    check ProductId, BrandId in UpdateManyModels

--- _1_Products/_5_Models/Commands/UpdateManyModelsCommand.cs
+++ _1_Products/_5_Models/Commands/UpdateManyModelsCommand.cs
@@ -36,10 +36,9 @@ public class UpdateManyModelsCommandHandler : IRequestHandler<UpdateManyModelsCo
         // get oldModels from db
         var oldModels = _crudService.GetByIds<Model>(command.ModifiedModels.Select(p => p.Id).ToList());
 
-        // get existedModelsTitles and modifiedModelsTitles
+        // get existedModelsTitles and modifiedModelsTitles and changedModelsTitles
         var existedModelsTitles = oldModels.Select(p => p.Title);
         var modifiedModelsTitles = command.ModifiedModels.Select(p => p.ModifiedEntity.Title);
-        // get changedModelsTitles
         var changedModelsTitles = modifiedModelsTitles
             .Where(title => !existedModelsTitles.Contains(title))
             .ToList();
@@ -52,6 +51,42 @@ public class UpdateManyModelsCommandHandler : IRequestHandler<UpdateManyModelsCo
                 _errorMessage = $"Models List was rejected, Some Titles are already existed.";
                 _logger.LogError(_errorMessage);
                 throw new HttpRequestException(_errorMessage, null, HttpStatusCode.Conflict);
+            }
+        }
+
+        // get existedProductIds and modifiedProductIds and changedProductIds
+        var existedProductIds = oldModels.Select(p => p.ProductId);
+        var modifiedProductIds = command.ModifiedModels.Select(p => p.ModifiedEntity.ProductId);
+        var changedProductIds = modifiedProductIds
+            .Where(id => !existedProductIds.Contains(id))
+            .ToList();
+        // if any ProductId changed check if it is not existed in db and then reject all inputs
+        if (changedProductIds.Count > 0)
+        {
+            var existedProducts = _dbService.GetList<Product>(p => changedProductIds.Contains(p.Id));
+            if (existedProducts.Count != changedProductIds.Count)
+            {
+                _errorMessage = $"one or more ProductId are not existed.";
+                _logger.LogError(_errorMessage);
+                throw new HttpRequestException(_errorMessage, null, HttpStatusCode.NotFound);
+            }
+        }
+
+        // get existedBrandIds and modifiedBrandIds and changedBrandIds
+        var existedBrandIds = oldModels.Select(p => p.BrandId);
+        var modifiedBrandIds = command.ModifiedModels.Select(p => p.ModifiedEntity.BrandId);
+        var changedBrandIds = modifiedBrandIds
+            .Where(id => !existedBrandIds.Contains(id))
+            .ToList();
+        // if any BrandId changed check if it is not existed in db and then reject all inputs
+        if (changedBrandIds.Count > 0)
+        {
+            var existedBrands = _dbService.GetList<Brand>(p => changedBrandIds.Contains(p.Id));
+            if (existedBrands.Count != changedBrandIds.Count)
+            {
+                _errorMessage = $"one or more BrandId are not existed.";
+                _logger.LogError(_errorMessage);
+                throw new HttpRequestException(_errorMessage, null, HttpStatusCode.NotFound);
             }
         }
 
